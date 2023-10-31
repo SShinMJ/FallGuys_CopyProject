11c760dcd7bbdd0b921173b839a0ae40aafd4fa8
SShinMJ <winny0678@gmail.com>
2023-10-31 17:28:02 +0900

    Add: 프로젝트 세팅(맵, 리소스 등)

---
+++ Assets/05.ImportAsset/etc/Scripts/DestroyZone.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyZone : MonoBehaviour
+{
+    public ParticleSystem bounce;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.other.gameObject);
+
+        bounce.Play();
+        bounce.transform.position = transform.position;
+    }
+}
