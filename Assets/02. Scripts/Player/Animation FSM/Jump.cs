202bb17c952e54d1beb07934a7b7f31efa98dd2c
SShinMJ <winny0678@gmail.com>
2023-11-08 23:26:47 +0900

    Fix: 플레이어 점프 오류 수정 완료
    - Jump 애니메이션이 두개 존재하여 stateBase 스크립트가 두번 실행되어 Jump가 두번 실행된 것. 애니메이션을 하나로 합치거나 스크립트를 하나만 두어 문제 해결.

--- Assets/02. Scripts/Player/Animation FSM/Jump.cs
+++ Assets/02. Scripts/Player/Animation FSM/Jump.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Jump : StateBase
 {
     // 점프 힘 크기
-    [SerializeField] float _jumpForce = 5.0f;
+    [SerializeField] float _jumpForce = 8.5f;
     float positionY;
 
     public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
@@ -17,29 +17,21 @@ public class Jump : StateBase
     {
         base.OnStateEnter(animator, stateInfo, layerIndex);
 
-        if(controller.pw.IsMine)
-        {
-            // 프로퍼티기 때문에 y값만 수정할 수 없다.
-            rigidbody.velocity = new Vector3(rigidbody.velocity.x,
-                                             0.0f,
-                                             rigidbody.velocity.z);
+        // 프로퍼티기 때문에 y값만 수정할 수 없다.
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x,
+                                         0.0f,
+                                         rigidbody.velocity.z);
 
 
-            // ForceMode(지속적인 힘이 들어올 때 사용).Impulse(충격. 순간적인 힘으로, 질량 * 속도)
-            // VelocityChange : 질량 상관없이 힘을 가할 때 사용
-            // 따라서 충격을 위쪽방향으로 _force만큼 준다.
-            rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-        }
+        // ForceMode(지속적인 힘이 들어올 때 사용).Impulse(충격. 순간적인 힘으로, 질량 * 속도)
+        // VelocityChange : 질량 상관없이 힘을 가할 때 사용
+        // 따라서 충격을 위쪽방향으로 _force만큼 준다.
+        rigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
     }
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         base.OnStateUpdate(animator, stateInfo, layerIndex);
-
-        //if (controller.isGrounded)
-        //{
-        //    ChangeState(animator, State.Move);
-        //}
 
         if (transform.position.y <= positionY - 3)
         {
